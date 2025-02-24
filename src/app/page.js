6ebafcb7116a116ccd9670620594e924f262f16b
sandeshJITHVAR <sandesh.deshmukh@jithvar.com>
2025-02-24 14:01:57 +0530

    fix again

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,23 @@
-// import Bollywood from "@/section/home/Bollywood";
-// import Categaroy from "@/section/home/Categaroy";
-// import CityNews from "@/section/home/CityNews";
+import Bollywood from "@/section/home/Bollywood";
+import Categaroy from "@/section/home/Categaroy";
+import CityNews from "@/section/home/CityNews";
 import HeroSection from "@/section/home/HeroSection";
-// import MoreStories from "@/section/home/MoreStories";
-// import VideoNews2 from "@/section/home/VideoNews2";
-// import VideosStories1 from "@/section/comon/VideosSories1";
-// import WebStories from "@/section/home/WebStories";
+import MoreStories from "@/section/home/MoreStories";
+import VideoNews2 from "@/section/home/VideoNews2";
+import VideosStories1 from "@/section/comon/VideosSories1";
+import WebStories from "@/section/home/WebStories";
 
 export default function Home() {
   return (
     <main className="">
       <HeroSection />
-      {/* <WebStories banner={true} />
+      <WebStories banner={true} />
       <Categaroy />
       <VideosStories1 />
       <Bollywood />
       <VideoNews2 />
       <CityNews />
-      <MoreStories /> */}
+      <MoreStories />
     </main>
   );
 }
